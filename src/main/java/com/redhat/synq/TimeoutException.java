3beaf647fe916b442613c26a3ae642877410945a
Alec Henninger <alechenninger@gmail.com>
2014-06-24 11:37:06 -0400

    Replace repo with settings.xml for travis maven deployment

--- src/main/java/com/redhat/synq/TimeoutException.java
+++
@@ -1,9 +0,0 @@
-package com.redhat.synq;
-
-public class TimeoutException extends SynqException {
-    private static final long serialVersionUID = 7194182399119358208L;
-    
-    public TimeoutException(Event<?> event) {
-        super("Timed out waiting for event to occur: " + event.toString());
-    }
-}
